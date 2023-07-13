67b24283e839cf2c627a24a5e10e2cc8b5290f1f
XuanTrieu <trieu.tx171830@sis.hust.edu.vn>
2023-07-13 02:28:17 -0700

    Euclidean Clustering with PCL Libra

--- SFND_Lidar_Obstacle_Detection/src/environment.cpp
+++ SFND_Lidar_Obstacle_Detection/src/environment.cpp
@@ -55,6 +55,20 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
         pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
     renderPointCloud(viewer, segmentCloud.first, "ostCloud", Color(0, 1, 0));
     renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(1, 0, 0));
+
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters =
+        pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(0.5, 0.5, 0), Color(0, 1, 0), Color(0, 0, 1)};
+
+    for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
+    {
+        std::cout << "cluster size ";
+        pointProcessor.numPoints(cluster);
+        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
+
+        ++clusterId;
+    }
 }
 
 // setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
