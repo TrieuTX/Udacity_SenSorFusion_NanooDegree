a7ee20c9925642ea368fa20c1888e480b70b7a0d
XuanTrieu <trieu.tx171830@sis.hust.edu.vn>
2023-07-13 02:50:22 -0700

    Load PCD

--- SFND_Lidar_Obstacle_Detection/src/environment.cpp
+++ SFND_Lidar_Obstacle_Detection/src/environment.cpp
@@ -74,6 +74,17 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
     }
 }
 
+void cityBlock(pcl::visualization::PCLVisualizer::Ptr &viewer)
+{
+    // ------------------------------------------------------------------
+    // -----Open 3D viewer and display City Block with single PCD   -----
+    // ------------------------------------------------------------------
+    ProcessPointClouds<pcl::PointXYZI> *pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud =
+        pointProcessorI->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
+    renderPointCloud(viewer, inputCloud, "inputcloud");
+}
+
 // setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
 void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
@@ -111,8 +122,8 @@ int main(int argc, char **argv)
     pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
-    simpleHighway(viewer);
-
+    // simpleHighway(viewer);
+    cityBlock(viewer);
     while (!viewer->wasStopped())
     {
         viewer->spinOnce();
