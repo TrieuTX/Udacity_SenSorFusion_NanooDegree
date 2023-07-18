bebc221662dd9ecbb125c90a36687d1b191d6581
XuanTrieu <trieu.tx171830@sis.hust.edu.vn>
2023-07-17 23:25:47 -0700

    Tracking Image Features Exercise 7. and 11.

--- SFND_Camera/Lesson 4 - Tracking Image Features/Gradient-based vs. Binary Descriptors/describe_keypoints/src/describe_keypoints.cpp
+++ SFND_Camera/Lesson 4 - Tracking Image Features/Gradient-based vs. Binary Descriptors/describe_keypoints/src/describe_keypoints.cpp
@@ -4,8 +4,9 @@
 #include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+//#include <opencv2/xfeatures2d.hpp>
+//#include <opencv2/xfeatures2d/nonfree.hpp>
 #include <opencv2/xfeatures2d.hpp>
-#include <opencv2/xfeatures2d/nonfree.hpp>
 
 using namespace std;
 
@@ -44,8 +45,28 @@ void descKeypoints1()
     // time for both steps and compare both BRISK and SIFT
     // with regard to processing speed and the number and
     // visual appearance of keypoints.
-}
+    detector = cv::xfeatures2d::SIFT::create();
+    vector<cv::KeyPoint> kptsSIFT;
 
+    t = (double)cv::getTickCount();
+    detector->detect(imgGray, kptsSIFT);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "SIFT detector with n= " << kptsSIFT.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    descriptor = cv::xfeatures2d::SiftDescriptorExtractor::create();
+    cv::Mat descSIFT;
+    t = (double)cv::getTickCount();
+    descriptor->compute(imgGray, kptsSIFT, descSIFT);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "SIFT descriptor in " << 1000 * t / 1.0 << " ms" << endl;
+
+    visImage = img.clone();
+    cv::drawKeypoints(img, kptsSIFT, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    windowName = "SIFT Results";
+    cv::namedWindow(windowName, 2);
+    imshow(windowName, visImage);
+    cv::waitKey(0);
+}
 int main()
 {
     descKeypoints1();
