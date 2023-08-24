ff5d3409396178a1e922f31ddde98a5ddc7d7e26
XuanTrieu <trieu.tx171830@sis.hust.edu.vn>
2023-08-24 02:39:07 -0700

    implement function ProcessMeasurement

--- SFND_Unscented_Kalman_Filter/src/ukf.cpp
+++ SFND_Unscented_Kalman_Filter/src/ukf.cpp
@@ -1,6 +1,6 @@
 #include "ukf.h"
 #include "Eigen/Dense"
-
+#include <iostream>
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -17,10 +17,10 @@ UKF::UKF()
 
     // initial state vector
     x_ = VectorXd(5);
-
+    x_.fill(0.0);
     // initial covariance matrix
     P_ = MatrixXd(5, 5);
-
+    x_.fill(0.0);
     // Process noise standard deviation longitudinal acceleration in m/s^2
     std_a_ = 30;
 
@@ -55,6 +55,9 @@ UKF::UKF()
      * TODO: Complete the initialization. See ukf.h for other member properties.
      * Hint: one or more values initialized above might be wildly off...
      */
+
+    // check first measurement
+    is_initialized_ = false;
 }
 
 UKF::~UKF()
@@ -67,6 +70,66 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package)
      * TODO: Complete this function! Make sure you switch between lidar and radar
      * measurements.
      */
+    if (!is_initialized_)
+    {
+        time_us_ = meas_package.timestamp_;
+
+        if (meas_package.sensor_type_ == MeasurementPackage::RADAR)
+        {
+            // std::cout << "Radar" << meas_package.raw_measurements_[0] << " " << meas_package.raw_measurements_[1] <<
+            // " "
+            //           << meas_package.raw_measurements_[2] << std::endl;
+            float rho = meas_package.raw_measurements_[0];
+            float phi = meas_package.raw_measurements_[1];
+            float rho_dot = meas_package.raw_measurements_[2];
+
+            float px = rho * cos(phi);
+            float py = rho * sin(phi);
+
+            x_ << px, py, 0, 0, 0;
+        }
+        if (meas_package.sensor_type_ == MeasurementPackage::LASER)
+        {
+            // std::cout << "Laser: " << meas_package.raw_measurements_[0] << " " << meas_package.raw_measurements_[1]
+            //           << std::endl;
+
+            float px = meas_package.raw_measurements_[0];
+            float py = meas_package.raw_measurements_[1];
+
+            if (fabs(px) < 0.001 && fabs(py) < 0.001)
+            {
+                px = 0.001;
+                py = 0.001;
+            }
+
+            x_ << px, py, 0, 0, 0;
+        }
+        P_ << 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, std_laspx_ * std_laspx_, 0, 0, 0, 0, 0,
+            std_laspy_ * std_laspy_;
+
+        time_us_ = meas_package.timestamp_;
+
+        is_initialized_ = true;
+        return;
+    }
+
+    double dt = (meas_package.timestamp_ - time_us_) / 1000000.0;
+    time_us_ = meas_package.timestamp_;
+    std::cout << "Prediction: " << std::endl;
+    std::cout << P_ << std::endl;
+    Prediction(dt);
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_)
+    {
+        std::cout << "UpdateRadar: " << std::endl;
+        UpdateRadar(meas_package);
+        std::cout << P_ << std::endl;
+    }
+    else if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_)
+    {
+        std::cout << "UpdateLidar: " << std::endl;
+        UpdateLidar(meas_package);
+        std::cout << P_ << std::endl;
+    }
 }
 
 void UKF::Prediction(double delta_t)
